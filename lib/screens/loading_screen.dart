05f83b68164896e2cc3fe4f33d63e539ca858ccb
Ralph Lee <ralph.lee@live.ca>
2021-02-21 23:46:46 +0900

    Add dart:convert package to convert json files

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -27,7 +28,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var id = jsonDecode(data).weather[0].id;
+
+      print(id);
     } else {
       print(response.statusCode);
     }

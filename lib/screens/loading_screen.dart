e7bdcd1c0806bf4de362930efacb898b18961e85
Ralph Lee <ralph.lee@live.ca>
2021-02-25 23:43:43 +0900

    Pass locationWeather prop inside of LocationScreen class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -35,7 +35,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     var weatherData = await networkHelper.getData();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen();
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
   }
 

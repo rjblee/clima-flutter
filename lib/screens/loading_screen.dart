e968d589485af4cf0e76e42201100880c2bfb13b
Ralph Lee <ralph.lee@live.ca>
2021-02-28 23:28:06 +0900

    Refactor out code for getting weather information to weather.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,6 @@
 import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = '100d57c558c04246e3f146c4fd4d2d12';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,15 +15,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
-
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
         locationWeather: weatherData,

1ae501004853d35f7ba1832ac713470d77fbe2a7
Ralph Lee <ralph.lee@live.ca>
2021-02-28 23:28:52 +0900

    Add getLocationWeather method from loading_screen.dart

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,23 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '100d57c558c04246e3f146c4fd4d2d12';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
